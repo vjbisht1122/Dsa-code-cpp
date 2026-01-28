c12428529cc98d3c95ac0a09d131b8a59b608925
vjbisht1122 <bishtvijay437@gmail.com>
2026-01-28 23:51:10 +0530

    practised these questions on 28 Jan 11.47pm

--- bulb-switcher.cpp
+++ bulb-switcher.cpp
@@ -6,5 +6,6 @@ int main(){
     cout<<"Enter the number of bulbs :"<<endl;
     cin>>bulbs;
     cout<<"The number of bulbs on after the iterations are : "<<sqrt(bulbs)<<endl;
+    //the main logic behind using this sqrt() function as each time when all the bulbs are iterated at the end always the perfect squareRoot numbered bulb are always on for example(1,4(2x2),9(3x3),16(4x4),25(5x5)) so we used this function
     return 0;
 }
